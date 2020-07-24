1b87412e53eab388050825f1b0a95bbd4b55c738
emanuelNastroga <emanuelnastroga@hotmail.com>
2020-07-23 21:38:41 -0300

    alteracoes em tabelas

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.models import Evento
+
+
+
+class Evento_admin(admin.ModelAdmin):
+    list_display = ('titulo','data_evento','data_criacao')
+    list_filter = ('titulo','usuario',)
+
+admin.site.register(Evento,Evento_admin)
